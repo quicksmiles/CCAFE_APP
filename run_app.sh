7b5bd0546d41db7681ef3bcb27bce28d5d31ee20
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-02-13 13:16:53 -0700

    Added script for building and running the app in a container

---
+++ run_app.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# this script first builds a Docker image for the project, then runs it,
+# attaching to the logs for the container. pressing ctrl-c in the terminal will
+# stop the container and remove it.
+
+# the first build will be slow (~13 minutes on my m1 macbook pro), but
+# subsequent builds should be fast thanks to docker's layer caching mechanism.
+
+# to access the site once the container is running, go to:
+# http://localhost:3850/ccafe/
+
+IMAGE_TAG=latest
+IMAGE=ccafe_app:${IMAGE_TAG}
+
+docker build --platform linux/amd64 -f ./docker/Dockerfile -t ${IMAGE} . &&
+docker run  --platform linux/amd64 --name ccafe_app --rm -it \
+    -p 3850:3838 \
+    -v $(pwd):/srv/shiny-server/app/ \
+    ${IMAGE}
