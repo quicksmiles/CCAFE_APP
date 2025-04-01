6891b539eeb1cc4f384f4cdac1a0371064300881
Hugo Lemus <victor.lemusgomez@ucdenver.edu>
2025-04-01 10:23:46 -0600

    update UI grid style. update estimate calculation

--- app/view/operation_selection.R
+++ app/view/operation_selection.R
@@ -1,158 +1,144 @@
 box::use(
   argonR[...],
   argonDash[...],
+  CCAFE[...],
   reactR[...],
   shiny[...],
   shinyjs[...],
-  DT[DTOutput, renderDT])
+  DT[DTOutput, renderDT],
+)
+
+box::use(
+  app/view/file_upload[...],
+)
 
 
 operationSelectionUI <- function(id) {
   ns <- NS(id)
   
-  argonRow(
-    argonColumn(
-      width = 12,
-      br(),
-      br(),
-      hidden(
-        div(id = ns("modules"),
-            argonCard(
-              title = "Data Selection",
-              width = 4,
-              radioButtons(
-                ns("data_source"),
-                label = "Choose data source:",
-                choices = c("Upload File" = "upload", "Use Sample Data" = "sample"),
-                selected = "upload",
-                inline = TRUE
-              ),
-              conditionalPanel(
-                condition = paste0("input['", ns("data_source"), "'] == 'upload'"),
-                fileInput(
-                  ns("file"),
-                  "Upload GWAS summary statistics (compressed text or VCF file)",
-                  accept = c(".bgz", ".gz"),
-                  buttonLabel = "Upload",
-                  width = "100%"
-                )
-              ),
-              actionButton(ns("process_file"), "Process Data", class = "btn btn-default btn-round")
+  tagList(
+    argonRow(
+      
+      # Left Column with Three Cards
+      argonColumn(
+        width = 4,
+        
+        # Data Selection Card
+        argonCard(
+          title = "Data Selection",
+          width = 12,
+          radioButtons(
+            ns("data_source"),
+            label = "Choose data source:",
+            choices = c("Upload File" = "upload", "Use Sample Data" = "sample"),
+            selected = "upload",
+            inline = TRUE
+          ),
+          
+          conditionalPanel(
+            condition = sprintf("input['%s'] == 'upload'", ns("data_source")),
+            fileUploadUI(ns("file_upload"))
+          ),
+          
+          conditionalPanel(
+            condition = sprintf("input['%s'] == 'sample'", ns("data_source")),
+            actionButton(ns("process_file"), "Process Data", class = "btn btn-default btn-round")
+          )
+        ),
+        
+        # Inputs Card
+        argonCard(
+          title = "Inputs",
+          width = 12,
+          
+          # Conditional Panel: Only display inputs when data is available
+          conditionalPanel(
+            condition = sprintf("input['%s'] != null || input['%s'] > 0", ns("file_upload"), ns("process_file")),
+            
+            # Operation Selection (Radio Buttons)
+            radioButtons(
+              ns("operation"), "Select Operation",
+              choices = c("CaseControl_AF" = "AF", "CaseControl_SE" = "SE"),
+              inline = TRUE
             ),
             
-            argonCard(
-              title = "Inputs",
-              width = 4,
-              conditionalPanel(
-                condition = paste0("input['", ns("operation"), "'] == 'AF'"),
-                numericInput(ns("N_case"), "Number of cases:", value = NULL),
-                numericInput(ns("N_control"), "Number of controls:", value = NULL),
-                selectInput(ns("OR_colname"), "Odds Ratio Column", choices = NULL),
-                selectInput(ns("AF_total_colname"), "Total AF Column:", choices = NULL),
-                actionButton(ns("run_casecontrolaf"), "Run CaseControl_AF", class = "btn-primary"),
-                downloadButton(ns("download_results"))
-              ),
-              conditionalPanel(
-                condition = paste0("input['", ns("operation"), "'] == 'SE'"),
-                selectInput(ns("super_population"), "Select Super-Population for Bias Correction",
-                            choices = c("total", "afr", "ami", "amr", "asj", "eas", "fin", "mid", "nfe", "remaining", "sas")),
-                numericInput(ns("N_case_se"), "Number of cases:", value = NULL),
-                numericInput(ns("N_control_se"), "Number of controls:", value = NULL),
-                selectInput(ns("OR_colname_se"), "Odds Ratio Column", choices = NULL),
-                selectInput(ns("SE_colname_se"), "SE Column:", choices = NULL),
-                selectInput(ns("chromosome_colname"), "Chromosome Column:", choices = NULL),
-                selectInput(ns("position_colname"), "Position Column:", choices = NULL),
-                textInput(ns("user_email"), "Email", placeholder = "Enter your email"),
-                actionButton(ns("run_casecontrolse"), "Run CaseControl_SE", class = "btn-primary")
-              )
+            # AF Section
+            conditionalPanel(
+              condition = sprintf("input['%s'] == 'AF'", ns("operation")),
+              numericInput(ns("N_case"), "Number of cases:", value = 16550),
+              numericInput(ns("N_control"), "Number of controls:", value = 403923),
+              selectInput(ns("OR_colname"), "Odds Ratio Column", choices = NULL),
+              selectInput(ns("AF_total_colname"), "Total AF Column:", choices = NULL),
+              actionButton(ns("run_casecontrolaf"), "Run CaseControl_AF", class = "btn-primary")
             ),
-            argonCard(
-              title = tags$div(
-                tags$span("Effect Estimate Calculation"),
-                style = "display: flex; justify-content: space-between; align-items: center;",
-                tags$label(
-                  class = "custom-toggle",
-                  tags$input(
-                    type = "checkbox",
-                    id = ns("toggle_button")
-                  ),
-                  tags$span(
-                    style = "display: flex; justify-content: space-between; align-items: center;",
-                    class = "custom-toggle-slider rounded-circle"
-                  )
-                )
-              ),
-              width = 4,
+            
+            # SE Section
+            conditionalPanel(
+              condition = sprintf("input['%s'] == 'SE'", ns("operation")),
+              selectInput(ns("population"), "Select Population for Bias Correction",
+                          choices = c("total", "afr", "ami", "amr", "asj", "eas", "fin", "mid", "nfe", "remaining", "sas")),
+              numericInput(ns("N_case_se"), "Number of cases:", value = NULL),
+              numericInput(ns("N_control_se"), "Number of controls:", value = NULL),
+              selectInput(ns("OR_colname_se"), "Odds Ratio Column", choices = NULL),
+              selectInput(ns("SE_colname_se"), "SE Column:", choices = NULL),
+              selectInput(ns("chromosome_colname"), "Chromosome Column:", choices = NULL),
+              selectInput(ns("position_colname"), "Position Column:", choices = NULL),
+              textInput(ns("user_email"), "Email", placeholder = "Enter your email"),
+              actionButton(ns("run_casecontrolse"), "Run CaseControl_SE", class = "btn-primary")
             )
+          )
         ),
-        DTOutput("data_table")
+        
+        # Effect Estimate Calculation Card
+        argonCard(
+          title = tags$div(
+            style = "display: flex; justify-content: space-between; align-items: center; width: 100%;",
+            
+            # Title text on the left
+            tags$span("Effect Estimate Calculation"),
+            
+            # Toggle button on the right
+            tags$label(
+              class = "custom-toggle",
+              style = "margin-left: 100px;",  # Pushes the toggle to the right
+              tags$input(
+                type = "checkbox",
+                id = ns("toggle_button")
+              ),
+              tags$span(
+                style = "display: inline-block; margin-left: auto;", 
+                class = "custom-toggle-slider rounded-circle"
+              )
+            )
+          ),
+          
+          width = 12,
+          
+          # Input Fields shown only when toggle is ON
+          conditionalPanel(
+            condition = sprintf("input['%s'] == true", ns("toggle_button")),
+            selectInput(ns("BETA_colname"), "Beta Column", choices = NULL)
+          )
+        )
+      ),
+      
+      # Right Column with Full-Height DTOutput
+      argonColumn(
+        width = 8,
+        argonCard(
+          width = 12,
+          # style = "min-height: 100%; height: calc(100vh - 100px);",  # Full height of the three cards
+          tags$div(class = "table-responsive py-4", DTOutput("sample_preview")),
+          DTOutput(ns("upload_preview")),
+          DTOutput(ns("results_preview")),
+          downloadButton(ns("download_results"), "Download Results")
+        )
       )
     )
   )
 }
 
-  
-  # argonRow(
-  #   argonColumn(
-  #     width = 4,
-  #     argonCard(
-  #       width = "auto",
-  #       title = "Upload & Download",
-  #       fileInput(ns("file"), "Upload your GWAS data (gzipped text or gzipped vcf files only)", accept = c(".bgz", ".gz")),
-  #       actionButton(ns("process_file"), "Process File"),
-  #       downloadButton(ns("download_results"), "Download Results")
-  #     )
-  #   ),
-  #   argonColumn(
-  #     width = 4,
-  #     argonCard(
-  #       width = "auto",
-  #       title = "Input Selection & Filtering",
-  #       # radioButtons(ns("operation"), "Select Operation",
-  #       #              choices = c("CaseControl_AF" = "AF", "CaseControl_SE" = "SE")),
-  #       argonRow(
-  #         div(
-  #           class = "btn-group-toggle",
-  #           type = "radio",
-  #           argonButton(ns("operation"), "Select Operation",
-  #                        choices = c("CaseControl_AF" = "AF", "CaseControl_SE" = "SE"))
-  #         )
-  #       ),
-  #       conditionalPanel(
-  #         condition = paste0("input['", ns("operation"), "'] == 'AF'"),
-  #         numericInput(ns("N_case"), "Number of cases:", value = NULL),
-  #         numericInput(ns("N_control"), "Number of controls:", value = NULL),
-  #         selectInput(ns("OR_colname"), "Odds Ratio Column", choices = NULL),
-  #         selectInput(ns("AF_total_colname"), "Total AF Column:", choices = NULL),
-  #         actionButton(ns("run_casecontrolaf"), "Run CaseControl_AF", class = "btn-primary")
-  #       ),
-  #       conditionalPanel(
-  #         condition = paste0("input['", ns("operation"), "'] == 'SE'"),
-  #         selectInput(ns("super_population"), "Select Super-Population for Bias Correction", 
-  #                     choices = c("total", "afr", "ami", "amr", "asj", "eas", "fin", "mid", "nfe", "remaining", "sas")),
-  #         numericInput(ns("N_case_se"), "Number of cases:", value = NULL),
-  #         numericInput(ns("N_control_se"), "Number of controls:", value = NULL),
-  #         selectInput(ns("OR_colname_se"), "Odds Ratio Column", choices = NULL),
-  #         selectInput(ns("SE_colname_se"), "SE Column:", choices = NULL),
-  #         selectInput(ns("chromosome_colname"), "Chromosome Column:", choices = NULL),
-  #         selectInput(ns("position_colname"), "Position Column:", choices = NULL),
-  #         textInput(ns("user_email"), "Email", placeholder = "Enter your email"),
-  #         actionButton(ns("run_casecontrolse"), "Run CaseControl_SE", class = "btn-primary")
-  #       ),
-  #       uiOutput(ns("dynamic_inputs"))
-  #     )
-  #   ),
-  #   argonColumn(
-  #     width = 4,
-  #     argonCard(
-  #       title = "Results",
-  #       width = "auto",
-  #       tableOutput(ns("file_preview")),
-  #       DTOutput(ns("results_table"))
-  #     )
-  #   )
-  # )
-  # 
+
   # 
   # tagList(
   #   h3("Choose Operation"),
@@ -186,7 +172,7 @@ operationSelectionUI <- function(id) {
   #   DTOutput(ns("results_preview"))
   # )
 
-operationSelectionServer <- function(id, uploaded_data, column_names, main_session) {
+operationSelectionServer <- function(id, main_session) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     results <- reactiveVal()
@@ -197,6 +183,35 @@ operationSelectionServer <- function(id, uploaded_data, column_names, main_sessi
       sampleDat
     })
     
+    # Render preview of uploaded data
+    output$sample_preview <- renderDT({
+      req(getSampleData())
+      
+      # Properly render DataTable with class and ID inside the datatable function
+      datatable(
+        getSampleData(),
+        
+        # DataTable options
+        options = list(
+          dom = 'Bfrtip',                       # Include buttons
+          buttons = c('download'),  # Export options
+          paging = TRUE,
+          searching = TRUE,
+          ordering = TRUE,
+          scrollX = TRUE,
+          autoWidth = TRUE
+        ),
+        
+        # Add extensions and styling
+        extensions = c('Buttons', 'Select'),
+        class = "table table-flush",
+        id = "datatable-basic", # Use Argon table styling
+        rownames = FALSE                        # Hide row names
+      )
+    })
+    
+    uploaded_data <- fileUploadServer("file_upload", main_session)
+    
     # Choose between uploaded data or sample data
     current_data <- reactive({
       if (input$data_source == "sample") {
@@ -206,14 +221,19 @@ operationSelectionServer <- function(id, uploaded_data, column_names, main_sessi
         uploaded_data()
       }
     })
+    
+    column_names <- reactive({
+      req(current_data())
+      colnames(current_data())
+    })
       
-    selected_super_population <- reactive({
-      input$super_population  # Automatically updates when user selects a value
+    selected_population <- reactive({
+      input$population  # Automatically updates when user selects a value
     })
     
     observeEvent(column_names(), {
       req(column_names())
-      req(uploaded_data())
+      req(current_data())
       
       updateSelectInput(session, "BETA_colname", choices = column_names(), selected = "BETA")
       updateNumericInput(session, "N_case", value = input$N_case)
@@ -222,28 +242,44 @@ operationSelectionServer <- function(id, uploaded_data, column_names, main_sessi
       updateNumericInput(session, "N_control_se", value = input$N_control_se)
       updateSelectInput(session, "OR_colname", choices = column_names(), selected = "OR")
       updateSelectInput(session, "OR_colname_se", choices = column_names(), selected = "OR")
-      updateSelectInput(session, "AF_total_colname", choices = column_names(), selected = "AF_total")
+      updateSelectInput(session, "AF_total_colname", choices = column_names(), selected = "true_maf_pop")
       updateSelectInput(session, "SE_colname_se", choices = column_names(), selected = "SE")
       updateSelectInput(session, "chromosome_colname", choices = column_names(), selected = "chrom")
       updateSelectInput(session, "position_colname", choices = column_names(), selected = "pos")
+      # updateSelectInput(session, "population", selected = "nfe")
     })
-    # observe if the calculate OR check box is selected
+    # observe if the calculate OR button is toggled
     # updated_data <- reactiveVal()
     observe({
-      req(input$BETA_colname)
-      df <- uploaded_data()
-      if(input$calculate_OR == TRUE){
-        df$OR <- exp(df[[input$BETA_colname]])
-        uploaded_data(df)
+      req(uploaded_data())  # Ensure data is uploaded first
+      
+      # Only proceed if toggle is ON
+      if (input$toggle_button) {
+        
+        # Get the dataframe
+        df <- uploaded_data()
+        
+        # Apply calculation if the Beta column is selected
+        if (!is.null(input$BETA_colname) && input$BETA_colname %in% colnames(df)) {
+          df$OR <- exp(df[[input$BETA_colname]])
+          uploaded_data(df)
+          
+          # Update the OR column selection
+          updateSelectInput(session, "OR_colname", choices = colnames(df), selected = "OR")
+        }
+        
+      } else {
+        # Optionally reset or clear values when toggle is OFF
+        updateSelectInput(session, "BETA_colname", choices = NULL)
+        updateSelectInput(session, "OR_colname", choices = NULL)
       }
-      updateSelectInput(session, "OR_colname", choices = colnames(df), selected = "OR")
     })
     
     
     observeEvent(input$run_casecontrolaf, {
-      req(uploaded_data())
+      req(current_data())
       
-      results_af <- CaseControl_AF(data = uploaded_data(),
+      results_af <- CaseControl_AF(data = current_data(),
                                    N_case = input$N_case,
                                    N_control = input$N_control,
                                    OR_colname = input$OR_colname,
@@ -257,11 +293,11 @@ operationSelectionServer <- function(id, uploaded_data, column_names, main_sessi
       # Move to email input page
       # updateNavbarPage(session, "CCAFE", selected = "Step3")
       req(uploaded_data())  # Ensure the user has uploaded data
-      req(selected_super_population())
+      req(selected_population())
       req(input$user_email)
       
       uploaded_data <- uploaded_data()
-      selected_population <- selected_super_population()
+      selected_population <- selected_population()
       
       user_email <- input$user_email
       N_case_se <- input$N_case_se
