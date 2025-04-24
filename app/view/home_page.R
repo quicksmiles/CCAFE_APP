bfe5b31ebd78813a8ff3e95545e4f2db978af390
Hugo Lemus <victor.lemusgomez@ucdenver.edu>
2025-04-24 10:13:49 -0600

    update contact in home page

--- app/view/home_page.R
+++ app/view/home_page.R
@@ -78,7 +78,7 @@ homeUI <- function(id) {
           background_color = "transparent",
           tags$div(
             style = "text-align: center;",
-            argonLead("Developed by: Your Name | Contact: your.email@example.com")
+            argonLead("Developed by: Hugo Lemus | Contact: ")
           )
         )
       )
