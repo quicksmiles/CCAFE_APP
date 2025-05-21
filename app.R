be23266b6ab88eceb1e2cfb0b672885a5a8e65f9
Hugo Lemus <victor.lemusgomez@ucdenver.edu>
2025-05-21 14:28:19 -0600

    include url link to live site

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 rhino::app()
 #
 # This is the CCAFE Shiny web application. You can run the application by clicking
-# the 'Run App' button in R Studio or by visiting [link].
+# the 'Run App' button in R Studio or by visiting https://hendrickslab.cu-dbmi.dev/ccafe/.
 #
 # Find out more about building applications with Shiny here:
 #
