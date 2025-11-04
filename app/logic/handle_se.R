8a051599a45c0293f6a2c4c0ecc559177946f80b
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-11-03 17:29:45 -0700

    Adds db methods. Saves job results to db + filesystem. Success emails now include link to statically-served job results archive. Failure emails now sent which include the error text.

--- app/logic/handle_se.R
+++ app/logic/handle_se.R
@@ -17,6 +17,10 @@ box::use(
 
 handle_se <- function(selected_population, user_email, uploaded_data, N_case_se, N_control_se, 
                          OR_colname_se, SE_colname_se, chromosome_colname, position_colname) {
+
+  # indicate that we've started the bg process
+  message("Starting ControlCase_SE method...")
+  
   # Call merge.R final_results function to obtain gnomAD population data
   super_population_results <- do_merge(uploaded_data, selected_population)
   selected_MAF_col <- paste0("MAF_", selected_population)
@@ -28,50 +32,47 @@ handle_se <- function(selected_population, user_email, uploaded_data, N_case_se,
   )
 
   # Perform CaseControl_SE operation
-  results_se <- CaseControl_SE(data = uploaded_data,
-                               N_case = N_case_se,
-                               N_control = N_control_se,
-                               OR_colname = OR_colname_se,
-                               SE_colname = SE_colname_se,
-                               chromosome_colname = chromosome_colname,
-                               position_colname = position_colname,
-                               do_correction = TRUE,
-                               correction_data = corr_data,
-                               sex_chromosomes = FALSE,
-                               remove_sex_chromosomes = TRUE
-  )
-  
-  save_results(results_se, user_email)
-  message("Saved SE results")
-  
-  # Read stored metadata
-  results_dir <- file.path(Sys.getenv("APP_ROOT", unset="../CCAFE"), "results")
-  metadata_file <- file.path(results_dir, "results_metadata.csv")
-  metadata <- utils::read.csv(metadata_file)
-  
-  # Only run when there are entries in metadata
-  if (nrow(metadata) > 0) {
-    # Track entries to remove
-    entries_to_keep <- metadata
-    
-    for (i in seq_len(nrow(metadata))) {
-      email <- metadata$email[i]
-      file_path <- metadata$file[i]
-      
-      if (send_email(email, file_path)) {
-        # Delete the file after successful email
-        file.remove(file_path)
-        message(paste("Deleted file:", file_path))
-        
-        # Remove entry from metadata
-        entries_to_keep <- entries_to_keep[-i, ]
-      }
-    }
-    
-    # Update metadata file (overwrite with remaining entries)
-    utils::write.csv(entries_to_keep, metadata_file, row.names = FALSE)
-  }
-  
-  return(results_se)
-  message("ControlCase_SE method was executed successfully!")
+  casecontrol_result <- tryCatch({
+    data <- CaseControl_SE(data = uploaded_data,
+                                N_case = N_case_se,
+                                N_control = N_control_se,
+                                OR_colname = OR_colname_se,
+                                SE_colname = SE_colname_se,
+                                chromosome_colname = chromosome_colname,
+                                position_colname = position_colname,
+                                do_correction = TRUE,
+                                correction_data = corr_data,
+                                sex_chromosomes = FALSE,
+                                remove_sex_chromosomes = TRUE
+    )
+
+    message("CaseControl_SE completed successfully.")
+
+    # save the job results to the db, giving us a UUID to email to the user
+    job_result_id <- save_results(data, user_email, TRUE)
+
+    message("Job results saved with ID: ", job_result_id)
+
+    # finally, send the success email with an embedded link to the results
+    send_results_email(user_email, job_result_id)
+
+    message("Results email sent to: ", user_email)
+
+    TRUE  # return TRUE on success
+  }, error = function(e) {
+    message("Error in CaseControl_SE: ", e$message)
+
+    # get the traceback as a string and write that out
+    tb <- utils::capture.output(traceback())
+    message("Traceback:\n", paste(tb, collapse = "\n"))
+
+    # send failure email
+    send_failure_email(user_email, e$message)
+
+    message("Failure email sent to: ", user_email)
+
+    FALSE  # return FALSE in case of error
+  })
+
+  return(casecontrol_result)
 }
